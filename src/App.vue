8e33da97a527d3e897700c41ee6f3508c856a803
truongvu <truongvu9221@gmail.com>
2022-10-01 13:45:30 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+  export default {
+  }
+</script>
+                npm run dev
+<template>
+  <h1>Hello, vue.js!</h1>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main> -->
+</template>
+
+<style>
+  .page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
